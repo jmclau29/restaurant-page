ee6191a532243435875d8c6dbb1a4073bc9b6676
Justin <jmclau29@gmail.com>
2024-08-22 12:30:02 +0900

    changes image names, restyles home page, imports all into index.js.

--- src/home.js
+++ src/home.js
@@ -1,17 +1,23 @@
-export default function homePageLoad() {
-    const content = document.getElementById(content);
+import signImage from './images/sign.jpg';
 
+function homePageLoad() {
     const heroBannerDiv = document.createElement('div');
+    heroBannerDiv.setAttribute('id', 'hero-banner');
     const heroBanner = document.createElement('div');
+    heroBanner.setAttribute('id', 'hero-banner');
     const heroBannerText = document.createElement('div');
+    heroBannerText.setAttribute('id', 'hero-banner-text');
     const heroTitle = document.createElement('h1');
+    heroTitle.setAttribute('id', 'hero-title');
     heroTitle.textContent = 'Delicious Japanese Food';
 
     const heroText = document.createElement('p');
+    heroText.setAttribute('id', 'hero-text');
     heroText.textContent = 'Enjoy delicious Japanese food at Japanese Restaurant DJF.';
 
     const heroImage = document.createElement('img');
-    heroImage.src = './images/remi-bertogliati-n4Uzf8nsYi0-unsplash.jpg';
+    heroImage.setAttribute('id', 'hero-image');
+    heroImage.src = signImage;
     heroImage.alt = 'Photo by Rémi Bertogliati at Unsplash';
 
     content.appendChild(heroBannerDiv);
@@ -22,41 +28,6 @@ export default function homePageLoad() {
     heroBannerText.appendChild(heroText);
 
     heroBanner.appendChild(heroImage);
-
-    const heroItems = document.createElement('div');
-
-    const heroItemOne = document.createElement('div');
-    const heroItemTwo = document.createElement('div');
-    const heroItemThree = document.createElement('div');
-
-    heroItems.appendChild(heroItemOne);
-
-    const heroItemOneImg = document.createElement('img');
-    heroItemOneImg.src = './images/flyd-loB9-eAYt2k-unsplash.jpg';
-    heroItemOneImg.alt = 'sushi';
-    const heroItemOneText = document.createElement('p');
-    heroItemOneText.textContent = 'Eat Sushi~';
-    heroItemOne.appendChild(heroItemOneImg);
-    heroItemOne.appendChild(heroItemOneText);
-
-    heroItems.appendChild(heroItemTwo);
-
-    const heroItemTwoImg = document.createElement('img');
-    heroItemTwoImg.src = './images/crystal-jo-TWTxHN2QIGc-unsplash.jpg';
-    heroItemTwoImg.alt = 'ramen';
-    const heroItemTwoText = document.createElement('p');
-    heroItemTwoText.textContent = 'Eat Ramen~';
-    heroItemTwo.appendChild(heroItemTwoImg);
-    heroItemTwo.appendChild(heroItemTwoText);
-
-    heroItems.appendChild(heroItemThree);
-
-    const heroItemThreeImg = document.createElement('img');
-    heroItemThreeImg.src = './images/seiya-maeda-H3wsvC2Tgk8-unsplash.jpg';
-    heroItemThreeImg.alt = 'natto';
-    const heroItemThreeText = document.createElement('p');
-    heroItemThreeText.textContent = 'Eat... Natto~';
-    heroItemThree.appendChild(heroItemThreeImg);
-    heroItemThree.appendChild(heroItemThreeText);
 }
 
+export default homePageLoad;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import homePageLoad from "./home.js";
 import "./styles.css";
 
+
+const navBar = document.querySelector('nav');
+const homeBtn = document.createElement('button');
+const menuBtn = document.createElement('button');
+const contactBtn = document.createElement('button');
+
+homeBtn.textContent = 'DJF Home';
+menuBtn.textContent = 'Menu';
+contactBtn.textContent = 'Contact';
+
+navBar.appendChild(homeBtn);
+navBar.appendChild(menuBtn);
+navBar.appendChild(contactBtn);
+
 homePageLoad();
