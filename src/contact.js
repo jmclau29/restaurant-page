b1c287bae19f5c45f23ace83ab472de09abf1e4b
Justin <jmclau29@gmail.com>
2024-08-24 19:53:43 +0900

    fixes an issue with opening the contact page, and changes the header background-color.

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,7 @@ function contactPageLoad() {
     content.textContent = '';
 
     const contactContainer = document.createElement('div');
-    contactContainer.setAttribute('id', 'menu-container');
+    contactContainer.setAttribute('id', 'contact-container');
     const contactTitleText = document.createElement('h1');
     contactTitleText.setAttribute('id', 'contact-title-text');
     contactTitleText.textContent = 'Contact us at:';
@@ -10,19 +10,22 @@ function contactPageLoad() {
     const contactList = document.createElement('ul');
     contactList.setAttribute('id', 'contact-list');
     const contactItemOne = document.createElement('li');
+    contactItemOne.textContent = 'Phone number: 123456789';
     const contactItemTwo = document.createElement('li');
+    contactItemTwo.textContent = 'E-mail: 123456789 @ 123456789';
     const contactItemThree = document.createElement('li');
+    contactItemThree.textContent = 'Social Media: 123456789 on Flipflop';
     
     contactItemOne.classList.add('contact-item');
     contactItemTwo.classList.add('contact-item');
     contactItemThree.classList.add('contact-item');
 
     content.appendChild(contactContainer);
-    menuContainer.appendChild(contactTitleText);
-    menuContainer.appendChild(contactList);
-    menu.appendChild(contactItemOne);
-    menu.appendChild(contactItemTwo);
-    menu.appendChild(contactItemThree);
+    contactContainer.appendChild(contactTitleText);
+    contactContainer.appendChild(contactList);
+    contactList.appendChild(contactItemOne);
+    contactList.appendChild(contactItemTwo);
+    contactList.appendChild(contactItemThree);
 }
 
 export default contactPageLoad;
